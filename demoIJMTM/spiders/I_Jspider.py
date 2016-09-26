7a4ff862952584b99c39b0c8dcab0f32018fe891
FengShi0705 <fs2813@ic.ac.uk>
2016-09-26 09:41:56 +0100

    W journals

--- demoIJMTM/spiders/I_Jspider.py
+++ demoIJMTM/spiders/I_Jspider.py
@@ -23,9 +23,9 @@ class I_Jspider(CrawlSpider):
 
     # filter to get English journals
     flt=SpdFun.EnJ()
-    #already finished up_A,B,C,D,E,F,G,H,up_I,up_J,K,L,up_M,N,O,P,Q,R,S,T,U journals
-    #processing V journals
-    flt.Eng_jnl('http://api.elsevier.com/sitemap/page/sitemap/v.html')
+    #already finished up_A,B,C,D,E,F,G,H,up_I,up_J,K,L,up_M,N,O,P,Q,R,S,T,U,V journals
+    #processing W journals
+    flt.Eng_jnl('http://api.elsevier.com/sitemap/page/sitemap/w.html')
     allowed_domains=['elsevier.com']
 
     #reject existing journals
